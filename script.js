426dc904ef9fbf1068e93821737619a8c0f4eeb9
Sebastian Boron <sebastian.boron8@gmail.com>
2022-11-29 01:17:00 +0100

    trello fixed post method

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
  $(document).ready(function() {
     const apiRoot = 'https://springwebappli-prod-kodilla-task-application-orfz23.mo6.mogenius.io/v1/tasks';
     const trelloApiRoot = 'https://trello.com/1/members/sebastianboron1/boards?fields=name,id&lists=all&key=c51cc4c716ff095e0241da18e14e1033&token=4e6ff5ae36f9e7459f81ade3fc782c3f5d3c91be0fa3232d1fadacb32031fe28';
-    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
+     const trelloApiRootCards = 'https://trello.com/1/members/sebastianboron1/cards?fields=name,id&lists=all&key=c51cc4c716ff095e0241da18e14e1033&token=4e6ff5ae36f9e7459f81ade3fc782c3f5d3c91be0fa3232d1fadacb32031fe28';
+     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
     var availableBoards = {};
@@ -166,7 +167,7 @@
     }
 
     function handleCardCreationRequest(event) {
-      var requestUrl = trelloApiRoot + '/cards';
+      var requestUrl = trelloApiRootCards
       var $relatedTaskRow = $(event.target).parents('[data-task-id]');
       var relatedTaskId = $relatedTaskRow.attr('data-task-id');
       var relatedTask = availableTasks[relatedTaskId];
